cb36cb235e5bb9dcfb8ae58ab18e6a00d96404d3
Mert Ozkan <f003hy9@dartmouth.edu>
2019-04-01 17:55:20 -0400

    ENH
    ENH: run_ddpopo_quest stores su info in .mat files and returns results once the session is completed

--- run_ddpopo_quest.m
+++ run_ddpopo_quest.m
@@ -12,18 +12,28 @@ if strcmp('test',init) || strcmp('try',init)
     f_nm = 'test.mat';    
 end
 f_nm = sprintf('%s/%s.mat',dr_dat,f_nm);
+
 if isfile(f_nm) && strcmp(f_nm,'test.mat')
     load(f_nm);
 else
-    trl_sq = setTrialOrder_inDDPOPOQuest(40);
+    trl_sq = setTrialOrder_inDDPOPOQuest(20);
     init_trl = 1;
-    qst = easy_quest('Create',4,.1,'StimulusTypes',[8,2,2]);
+    qst = easy_quest('Create',1,.1,'StimulusTypes',[8,2,2]);
 end
 
-[qst, trl_sq, init_trl] = ddpopo_quest(qst, trl_sq, init_trl);
+if ~(init_trl > length(trl_sq))
+    [qst, trl_sq, init_trl] = ddpopo_quest(qst, trl_sq, init_trl);
+else
+    error('Data collection for this participant seem to have completed.')
+end
 sca;
 
 save(f_nm, 'qst', 'trl_sq', 'init_trl')
+if init_trl > length(trl_sq)
+    disp('Quest session finalised');
+    op = easy_quest('Result',qst);
+    save(f_nm, 'op', '-append');
+end
 end
 %%
 function trlX = setTrialOrder_inDDPOPOQuest(qTrl_perStim)
